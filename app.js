536fca1196c21a38d539c19c5141d230109ba324
Eliya Gervas <eliyagervas27@gmail.com>
2022-07-13 01:43:26 +0300

    changes

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const PORT = 6000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Now look this COMPUTER IS SHIT!!!D TEST AGAIN again");
+  res.send(
+    "Now look this COMPUTER IS SHIT!!!D TEST AGAIN again shit about to happen"
+  );
 });
 
 app.listen(PORT, () => {
