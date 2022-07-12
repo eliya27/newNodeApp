14bdd3fa3a4e0aec929374e9ca312bc212c27790
Eliya Gervas <eliyagervas27@gmail.com>
2022-07-13 01:23:28 +0300

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const PORT = 6000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Now look this COMPUTER IS SHIT!!!D");
+  res.send("Now look this COMPUTER IS SHIT!!!D TEST AGAIN");
 });
 
 app.listen(PORT, () => {
